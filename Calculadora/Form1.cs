8bc33500bbe0ed9da08f8d2bfa3216372e5cc325
Paulo Coto <Paulo Coto@DESKTOP-QBHA61I>
2019-11-05 21:49:22 +0000

    testes

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -1,13 +1,6 @@
 ﻿// Calculadora 511
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculadora
@@ -19,18 +12,64 @@ namespace Calculadora
             InitializeComponent();
         }
 
+        string operador = "";
+        double visor = 0, mem = 0, num1 = 0, num2 = 0;
+        bool limpar = true;
+        private void mostra()
+        {
+            label1.Text = Math.Abs(visor).ToString();
+            if (mem != 0) memory.Visible = false; else memory.Visible = true;
+            if (visor < 0) minus.Visible = false; else minus.Visible = true;
+        }
         private void Clicar(object sender, EventArgs e)
         {
-            switch (((Button)sender).Text)
+            if (limpar) { label1.Text = ""; limpar = false; }
+            string botao = ((Button)sender).Text;
+            switch (botao)
             {
-                case "AC":
-                case "C": label1.Text = ""; break;
-                default:
+                case "M+": mem += visor; mostra(); break;
+                case "M-": mem -= visor; mostra(); break;
+                case "MRC": visor = mem; mostra(); break;
+                case "AC": label1.Text = ""; visor = 0; mem = 0; mostra(); break;
+                case "C": label1.Text = ""; visor = 0; mostra(); break;
+                case "+/-": visor = -visor; mostra(); break;
+                case "+":
+                case "-":
+                case "x":
+                case "/": operador = botao; num1 = visor; visor = 0; limpar = true; break;
+                case "=":
                     {
-                        label1.Text += ((Button)sender).Text;
+                        num2 = visor;
+                        if (operador == "+") visor = num1 + num2;
+                        else if (operador == "-") visor = num1 - num2;
+                        else if (operador == "x") visor = num1 * num2;
+                        else if (operador == "/") visor = num1 / num2;
+                        mostra();
                         break;
                     }
-
+                case ".":
+                    {
+                        string backup = label1.Text;
+                        mostra();
+                        try
+                        {
+                            label1.Text += ",";
+                            visor = Convert.ToDouble(label1.Text);
+                        }
+                        catch
+                        {
+                            Console.Beep();
+                            label1.Text = backup;
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        label1.Text += botao;
+                        visor = Convert.ToDouble(label1.Text);
+                        mostra();
+                        break;
+                    }
             }
         }
     }
