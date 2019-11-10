dacbb9b21d53f389cd880260cee554c0f834c9a0
Paulo Coto <Paulo Coto@DESKTOP-QBHA61I>
2019-11-10 12:05:05 +0000

    simplex

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -1,32 +1,13 @@
-﻿// Calculadora 511
-
-using System;
-using System.Drawing;
-using System.Drawing.Text;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Calculadora
 {
     public partial class Form1 : Form
     {
-        private readonly PrivateFontCollection private_fonts = new PrivateFontCollection();
         public Form1()
         {
             InitializeComponent();
-
-            String fonte = "Calculadora.Resources.digital-7-mono.ttf";
-            Stream fontStream = this.GetType().Assembly.GetManifestResourceStream(fonte);
-            IntPtr data = Marshal.AllocCoTaskMem((int)fontStream.Length);
-            byte[] fontdata = new byte[fontStream.Length];
-            fontStream.Read(fontdata, 0, (int)fontStream.Length);
-            Marshal.Copy(fontdata, 0, data, (int)fontStream.Length);
-            private_fonts.AddMemoryFont(data, (int)fontStream.Length);
-            fontStream.Close();
-            Marshal.FreeCoTaskMem(data);
-            label1.Font = new Font(private_fonts.Families[0], 43);
-            label1.UseCompatibleTextRendering = true;
         }
 
         string operador;
@@ -53,19 +34,28 @@ namespace Calculadora
 
         private void calcula()
         {
-            if (num2 == 0) num2 = visor;
+            if (num1 == 0) num1 = visor;
+            else num2 = visor;
             if (operador == "+") visor = num1 + num2;
             else if (operador == "-") visor = num1 - num2;
             else if (operador == "×") visor = num1 * num2;
             else if (operador == "÷") visor = num1 / num2;
+            else if (operador == "=") visor = num1;
             mostra();
             num1 = visor;
-            visor = 0;
             limpar = true;
+            visor = 0;
+        }
+        private void adiciona(string botao)
+        {
+            if (limpar) { label1.Text = "0"; limpar = false; }
+            if (label1.Text == "0") label1.Text = botao;
+            else label1.Text += botao;
+            if (label1.Text.Length > 9) label1.Text = label1.Text.Substring(0, 9);
+            visor = Convert.ToDouble(label1.Text);
         }
         private void Clicar(object sender, EventArgs e)
         {
-            if (limpar) { label1.Text = ""; limpar = false; }
             string botao = ((Button)sender).Tag.ToString();
             switch (botao)
             {
@@ -82,23 +72,9 @@ namespace Calculadora
                 case "-":
                 case "×":
                 case "÷":
-                    {
-                        if (num1 == 0) num1 = visor;
-                        else calcula();
-                        operador = botao;
-                        visor = 0;
-                        limpar = true;
-                        break;
-                    }
-                case "=": calcula(); break;
+                case "=": calcula(); operador = botao; break;
                 case ".": if (!label1.Text.Contains(",")) label1.Text += ","; break;
-                default:
-                    {
-                        label1.Text += botao;
-                        visor = Convert.ToDouble(label1.Text);
-                        mostra();
-                        break;
-                    }
+                default: adiciona(botao); break;
             }
         }
     }
